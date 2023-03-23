030b18be864fadb7e9a06dd9871774fb613b072f
cqDev <claudia.queipog@gmail.com>
2023-03-23 18:25:56 -0400

    Added actions folder

---
+++ rasa_bot/actions/actions.py
@@ -0,0 +1,40 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import sqlite3
+
+class ExtractAction(Action):
+    def name(self) -> Text:
+        return "action_test"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+
+        dispatcher.utter_message(text="Esto es una prueba de una RASA Action!")
+        return []
+
+# class ExtractAction(Action):
+#     def name(self) -> Text:
+#         return "action_test"
+
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         texto = tracker.latest_message.get("text")
+#         conn = sqlite3.connect("prueba.db")
+
+#         cursor = conn.cursor()
+
+#         cursor.execute("INSERT INTO no_answer(question) VALUES(?)", (texto,))
+#         conn.commit()
+
+#         conn.close()
+
+#         dispatcher.utter_message(text="Escribiste {}. Verdad? ".format(texto))
+#         return []
+
+
+
+
